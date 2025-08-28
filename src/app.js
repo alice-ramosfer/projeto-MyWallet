81e7b9f0b689bd6d3ef5d6d3dd1d52dadbe7c0c0
alice <aliceramosfer@hmail.com>
2025-08-28 15:23:41 -0300

    corrigindo as importações do app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv"
+import authRouter from "./routers/auth-router.js";
+import transactionsRoutes from "./routers/transactions-router.js";
 
 dotenv.config();
 
 const app = express();
 app.use(json());
 app.use(cors());
-
+app.use(authRouter);
+app.use(transactionsRoutes)
 
 
 const port = process.env.PORT || 5000;
